4ff1630c2740e8ee8b638ace81f8f917f5bdbd73
sngom1995 <ngomsamba75@gmail.com>
2024-07-10 18:20:34 +0000

    add schemaValidation for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,16 @@ import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
