fbc75c97ee59283104ac85f12c1d5f503dbe38d6
sngom1995 <ngomsamba75@gmail.com>
2024-07-13 00:20:02 +0000

    implement async dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,25 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import * as process from 'process';
 import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 
+import * as process from 'process';
+
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: undefined,
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
       validationSchema: Joi.object({
@@ -21,15 +34,7 @@ import appConfig from './config/app.config';
       }),
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-    }),
+
     CoffeeRatingModule,
     DatabaseModule,
   ],
